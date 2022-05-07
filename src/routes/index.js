aaba51d52a87a57c2ed21b6870dad60401fb34cc
hoanv.ptit <hoanv.ptit@gmail.com>
2022-05-07 23:29:39 +0700

    config router and build layout

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import { HeaderOnly } from '~/components/Layout';
+
+// không cần đăng nhập mới vào đc
+const publicRoutes = [
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/profile', component: Profile },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+//cần đăng nhập
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
